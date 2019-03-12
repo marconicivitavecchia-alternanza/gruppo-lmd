8643744c552936aece68aeaa0d25a479db99aa68
Your Name <you@example.com>
2019-03-12 08:40:15 +0200

    add javascript

---
+++ code/app.js
@@ -0,0 +1,8 @@
+var init = function(){
+
+    console.log("ciao sono tuba");
+    console.log($("title") .text("Marconi alternanza prova"));
+    $("#username>input").attr("placeholder","wbigger");
+}
+
+$(document).ready(init);

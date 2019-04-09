6046e5d5038bb0efb9440d88ca5e04ee0cb2f651
Your Name <you@example.com>
2019-04-09 07:48:31 +0300

    cambio

--- code/app.js
+++ code/app.js
@@ -1,8 +1,16 @@
-var init = function(){
-
-    console.log("ciao sono tuba");
-    console.log($("title") .text("Marconi alternanza prova"));
-    $("#username>input").attr("placeholder","wbigger");
+var accedi = function(){
+ console.log("hai cliccato su accedi");
+ var username=$("#username>input").val();
+ console.log("username"+username);
+ var password =$("#password>input").val();
+ console.log("password"+password);
 }
+    var init = function(){
+        console.log("ciao sono bonzo");
+        console.log($("title") .text("Marconi alternanza"));
+        $("#username>input").attr("placeholder","wbigger");
+        $("#submit>button").on("click",accedi);
+    }
+
 
 $(document).ready(init);
